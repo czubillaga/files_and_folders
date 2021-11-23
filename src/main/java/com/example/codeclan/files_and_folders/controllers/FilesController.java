b1e9c1ab388a1ff2b79ac1a19de29b00208a8db9
carloszubillaga <car.zubillaga@gmail.com>
2021-11-23 15:37:04 +0000

    Get paths done in each class

---
+++ src/main/java/com/example/codeclan/files_and_folders/controllers/FilesController.java
@@ -0,0 +1,29 @@
+package com.example.codeclan.files_and_folders.controllers;
+
+import com.example.codeclan.files_and_folders.models.File;
+import com.example.codeclan.files_and_folders.repositories.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class FilesController {
+
+    @Autowired
+    FileRepository fileRepository;
+
+    @GetMapping(value = "/files")
+    public ResponseEntity<List<File>> getAllFiles() {
+        return new ResponseEntity<>(fileRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/files/{id}")
+    public ResponseEntity getFile(@PathVariable Long id) {
+        return new ResponseEntity<>(fileRepository.findById(id), HttpStatus.OK);
+    }
+}
